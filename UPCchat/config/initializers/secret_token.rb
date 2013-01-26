7ac6e9cf8a01bbf90e145dfb680d42f76465cd32
Daniel <daalsuol@gmail.com>
2013-01-26 13:20:50 -0500

    UPCchat

---
+++ UPCchat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UPCchat::Application.config.secret_token = '29b8853d061716624ad8349765bf123b19e08046c7c570f3b27fd5c58753df2eb4ba8f923206b331b4825ceb4bb0609c42c5a5d20ceadc7cf1171a19f61cd86a'
